8a3b30d9fa0d65ee44dec66594f1887461f9f1ad
winkdoubleguns <winkimac@tims-iMac.local>
2021-04-26 18:57:55 -0500

    verb conjugation

--- grails-app/assets/javascripts/verbdeconstruction.js
+++ grails-app/assets/javascripts/verbdeconstruction.js
@@ -11,12 +11,13 @@ function newWholeWord(word, isSyllabary) {
         syllabary: "",
         phonetic: "",
         tmpParse: "",
+        rootPhonetic: "",
+        rootSyllabary: "",
         initialPrefixes: [],
         pronounPrefixes: [],
         reflexivePrefix: false,
         nonFinalSuffixes: [],
-        verbTenseSuffix: '',
-        verbTenseType: Tense.PRESENT,
+        verbTense: {verbEnding: "", verbType: ""},
         finalSuffixes: []
     };
 
@@ -39,15 +40,14 @@ async function deconstruct(wholeWord, word) {
     // look up in database
     let tmpValSyllabary = tsalagiToSyllabary(wholeWord.tmpParse);
     let values = await lookupWordInCED(tmpValSyllabary);
-    let displayObject = createDisplayObect();
 
     // if there's a result then we are going to put that information into the display object
     if (values.length > 0) {
         console.log("second value thing found");
         wholeWord.syllabary = tmpValSyllabary;
         // TODO: we found an item in the database - we need to create a new display object that we're going to send to the user or the page
-        displayObject = valueFound(values, wholeWord);
-        console.log("displayObject 2nd " + JSON.stringify(displayObject));
+        wholeWord = valueFound(values, wholeWord);
+        console.log("displayObject 2nd " + JSON.stringify(wholeWord));
     } else {
         wholeWord = getVerbTenseSuffixes(wholeWord);
 
@@ -62,10 +62,10 @@ async function deconstruct(wholeWord, word) {
         // wholeWord = getReflexivePrefix(wholeWord);
     }
 
-    return [wholeWord, displayObject];
+    return wholeWord;
 }
 
-function valueFound(values, displayObject) {
+function valueFound(values, wholeWord) {
     // TODO: what do we need from the dictionary entry?  definition,
     // TODO: could also be an array of values
     for (const value of values) {
@@ -76,29 +76,10 @@ function valueFound(values, displayObject) {
 
         console.log(jsonParsedValue);
 
-        displayObject.definition = jsonParsedValue.definitiond;
+        wholeWord.definition = jsonParsedValue.definitiond;
     }
 
-    return displayObject;
-}
-
-function createDisplayObect() {
-    var displayObject = {
-        syllabary:'',
-        phonetic: '',
-        definition: '',
-        word_type: '',//verb, noun, prefix, etc
-    };
-
-    return displayObject;
-}
-
-function populateDisplayObject(displayObject, wholeWord) {
-    displayObject.syllabary = wholeWord.syllabary;
-    // console.log("wholeWord " + JSON.stringify(wholeWord));
-    console.log(displayObject);
-
-    return displayObject;
+    return wholeWord;
 }
 
 //if processing phonetic then pass in isSyllabary as false
@@ -107,33 +88,27 @@ async function process(word, isSyllabary=true) {
 
     // look up in database
     let values = await lookupWordInCED(word);
-    let displayObject = createDisplayObect();
 
     // if there's a result then we are going to put that information into the display object
     if (values.length > 0) {
         console.log("first value thing found");
         // TODO: we found an item in the database - we need to create a new display object that we're going to send to the user or the page
-        displayObject = valueFound(values, wholeWord);
+        wholeWord = valueFound(values, wholeWord);
     } else { // if there's no result then we need to parse the word - it could be an adj, noun, or verb
         // breakdown the word
-        var tmpList = await deconstruct(wholeWord, word);
-        wholeWord = tmpList[0];
-        displayObject = tmpList[1];
-        console.log("displayObject " + JSON.stringify(displayObject));
+        wholeWord = await deconstruct(wholeWord, word);
 
-        if (displayObject.definition === "") {
-            displayObject = await hold(displayObject, wholeWord);
+        if (wholeWord.definition === "") {
+            wholeWord = await hold(wholeWord);
         }
     }
 
-    displayObject = populateDisplayObject(displayObject, wholeWord);
-
-    return displayObject;
+    return wholeWord;
 }
 
 async function display(word, isSyllabary=true) {
-    var displayObject = await process(word, isSyllabary);
-    console.log(JSON.stringify(displayObject));
+    var wholeWord = await process(word, isSyllabary);
+    console.log(JSON.stringify(wholeWord));
     // document.getElementById("display").innerText = JSON.stringify(wholeWord);
     // document.getElementById("syllabary").innerText = JSON.stringify(wholeWord.syllabary);
     // document.getElementById("phonetic").innerText = JSON.stringify(wholeWord.phonetic);
@@ -147,11 +122,11 @@ async function display(word, isSyllabary=true) {
 }
 
 function getBreakdown(word) {
-    var displayObject = process(word, true);
-    console.log(JSON.stringify(displayObject));
+    var wholeWord = process(word, true);
+    console.log(JSON.stringify(wholeWord));
 }
 
-async function hold(displayObject, wholeWord) {
+async function hold(wholeWord) {
     // right here put together a verb and then look it up
     if (wholeWord?.pronounPrefixes?.length > 0) {
         var pronPrefix = PronominalPrefixes.get(wholeWord.pronounPrefixes[0]);
@@ -165,44 +140,43 @@ async function hold(displayObject, wholeWord) {
             console.log("wholeWord prefixes " + JSON.stringify(wholeWord));
         }
 
+        //TODO: at this point uwoniha becomes uw + onih + a -- it should be u + wonih + a
+        if (pronPrefix !== undefined) {
+            if (pronPrefix.startsWith("uw")) {
+                if (wholeWord.pronounPrefixes[0].endsWith("B")) {
+                    pronPrefix = "ga";
+                }
 
-    //TODO: at this point uwoniha becomes uw + onih + a -- it should be u + wonih + a
-    if (pronPrefix !== undefined) {
-        if (pronPrefix.startsWith("uw")) {
-            if (wholeWord.pronounPrefixes[0].endsWith("B")) {
-                pronPrefix = "ga";
+                wholeWord.tmpParse = "w" + wholeWord.tmpParse;
+            } else if (pronPrefix.startsWith("u")) {
+                if (wholeWord.pronounPrefixes[0].endsWith("B")) {
+                    pronPrefix = "ga";
+                }
             }
-
-            wholeWord.tmpParse = "w" + wholeWord.tmpParse;
-        } else if (pronPrefix.startsWith("u")) {
-            if (wholeWord.pronounPrefixes[0].endsWith("B")) {
-                pronPrefix = "ga";
-            }
+        } else {
+            pronPrefix = "";
         }
-    } else {
-        pronPrefix = "";
+
+        if (wholeWord.tmpParse.endsWith("s")) {
+            wholeWord.tmpParse = wholeWord.tmpParse.substring(0, wholeWord.tmpParse.length -1) + "h";
+        } else /*if (!endsWithVowel(wholeWord.tmpParse))*/{
+            // wholeWord.tmpParse += 'a';
+        }
+
+        // once parsed then try another lookup in the database
+        var tmpWord = pronPrefix + wholeWord.tmpParse + VerbTenseLookup.get("PRESENT");
+        // console.log("tmpWord " + tmpWord);
+        tmpWord = tsalagiToSyllabary(tmpWord);
+        values = await lookupWordInCED(tmpWord);
+
+        // if there's a result in the database put the information into the display object
+        if (values.length > 0) {
+            // TODO:  also take broken down word and put that data into a display object
+            wholeWord = valueFound(values, wholeWord);
+        } else {// TODO: if there's no result then DO SOMETHING ELSE
+            console.log("still could not find a result");
+        }
     }
 
-    if (wholeWord.tmpParse.endsWith("s")) {
-        wholeWord.tmpParse = wholeWord.tmpParse.substring(0, wholeWord.tmpParse.length -1) + "h";
-    } else /*if (!endsWithVowel(wholeWord.tmpParse))*/{
-        // wholeWord.tmpParse += 'a';
-    }
-
-    // once parsed then try another lookup in the database
-    var tmpWord = pronPrefix + wholeWord.tmpParse + VerbTenseLookup.get("PRESENT");
-    // console.log("tmpWord " + tmpWord);
-    tmpWord = tsalagiToSyllabary(tmpWord);
-    values = await lookupWordInCED(tmpWord);
-
-    // if there's a result in the database put the information into the display object
-    if (values.length > 0) {
-        // TODO:  also take broken down word and put that data into a display object
-        displayObject = valueFound(values, displayObject);
-    } else {// TODO: if there's no result then DO SOMETHING ELSE
-        console.log("still could not find a result");
-    }
-    }
-
-    return displayObject;
+    return wholeWord;
 }

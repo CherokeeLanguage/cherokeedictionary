8a2011a60dd0893a751fe1cb5a12913a8e3cde8e
winkdoubleguns <winkimac@tims-iMac.local>
2021-05-03 10:49:59 -0500

    more improvements

--- grails-app/controllers/cherokeedictionary/ConjugationController.groovy
+++ grails-app/controllers/cherokeedictionary/ConjugationController.groovy
@@ -32,9 +32,9 @@ class ConjugationController {
         }
     }
 
-    def parser() {
+    def parser() {}
 
-    }
+    def verbPrefixTable() {}
 
     def getDisplayValue(params) {
         if (useV2) {

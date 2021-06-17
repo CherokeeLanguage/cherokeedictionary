2c87473420167a4b629568717d4db73dab913cd4
standtrooper <standtrooper@gmail.com>
2021-06-17 15:52:15 -0500

    adding verses

--- grails-app/controllers/cherokeedictionary/DigoweliController.groovy
+++ grails-app/controllers/cherokeedictionary/DigoweliController.groovy
@@ -1,8 +1,18 @@
 package cherokeedictionary
 
+import cherokee.corpus.Verse
+
 class DigoweliController {
 
     def index() {}
 
     def parameters() {}
+
+    def parse() {
+        def MAT_1_1 = new Verse(Book: "MAT", Chapter: "1", Verse: "1", LineMark: "", Context: "Ꭿ̲Ꭰ ᎪᏪᎵ ᎧᏃᎮᎭ ᏧᏁᏢᏔᏅᏒ Ꮵ̲Ꮜ Ꭶ̲ᎶᏁᏛ, Ꮥ̲Ꮻ ᎤᏪᏥ, Ꭱ̲ᏆᎭᎻ ᎤᏪᏥ.", source: "chr")
+        def MAT_1_2 = new Verse(Book: "MAT", Chapter: "1", Verse: "2", LineMark: "", Context: "Ꭱ̲ᏆᎭᎻ Ꭱ̲ᏏᎩ ᎤᏕᏁᎴᎢ; Ꭱ̲ᏏᎩᏃ Ꮴ̲ᎦᏈ ᎤᏕᏁᎴᎢ, Ꮴ̲ᎦᏈᏃ Ꮷ̲Ꮣ ᎠᎴ ᎾᏍᎩ ᎠᎾᏓᏅᏟ ᎬᏩᏕᏁᎴᎢ;", source: "chr")
+        MAT_1_1.save()
+        MAT_1_2.save()
+
+    }
 }
---
+++ grails-app/domain/cherokee/corpus/Verse.groovy
@@ -0,0 +1,15 @@
+package cherokee.corpus
+
+class Verse {
+
+    static constraints = {
+    }
+
+    String Book
+    String Chapter
+    String Verse
+    String LineMark
+    String Context
+
+    String source
+}
